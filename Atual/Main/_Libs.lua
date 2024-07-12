c95364d564059dfa22dd7064e83c9b0e0ae5ddfa
Paintaylor Projects <mflp55@hotmail.com>
2024-07-12 10:52:47 -0300

    Update _Libs.lua

--- Atual/Main/_Libs.lua
+++ Atual/Main/_Libs.lua
@@ -23,8 +23,8 @@ setvocsasuke = function()
   storage.magiacombo1 = 'Indoranoya'
   storage.magiacombo2 = 'Hono-Ikazuchi'
   storage.magiacombo3= 'Fureimurappufaia'
-  storage.magiacombo4 = 'Senjutsu no susanoo'
-  storage.magiacombo5 = 'Kurohomura no maiso'
+  storage.magiacombo4 = 'Kurohomura no maiso'
+  storage.magiacombo5 = ''
   storage.special = 'Utsushi wa me no kyosei'
   storage.ultimate = 'Susanoo Kagutsuchi'
   storage.fugaspell = 'Senjutsu no susanoo'
@@ -34,11 +34,11 @@ end
 
 
 setvocchino = function()
-  storage.magiacombo1 = 'Ketsuryugan'
-  storage.magiacombo2 = 'Chi no tatsumaki'
-  storage.magiacombo3= 'Buraddokuro'
-  storage.magiacombo4 = 'Raizu obu za Buraddo doragon'
-  storage.magiacombo5 = 'Genjutsu torappu'
+  storage.magiacombo1 = 'Chi no tatsumaki'
+  storage.magiacombo2 = 'Buraddokuro'
+  storage.magiacombo3= 'Raizu obu za Buraddo doragon'
+  storage.magiacombo4 = 'Genjutsu torappu'
+  storage.magiacombo5 = ''
   storage.special = 'Genjutsu: Ketsuryugan'
   storage.ultimate = 'Ketsueki kanri'
   storage.fugaspell = nil
