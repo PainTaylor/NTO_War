604b45ade8ac5ebe808379cf56c9dce2d9604b35
Paintaylor Projects <mflp55@hotmail.com>
2024-07-22 14:45:58 -0300

    Update NetLoader.lua

--- NetLoader.lua
+++ NetLoader.lua
@@ -23,3 +23,6 @@ modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/NTO_War/m
 modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/NTO_War/main/Atual/Main/Tools.lua', function(script)
     assert(loadstring(script))()
   end);
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/NTO_War/main/Atual/Main/RemoteControl.lua', function(script)
+    assert(loadstring(script))()
+  end);
