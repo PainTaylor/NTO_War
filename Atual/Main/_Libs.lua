b5d2e10afc6175d1c3a24306cf00bede822fae7c
Paintaylor Projects <mflp55@hotmail.com>
2024-07-23 09:44:29 -0300

    Update _Libs.lua

--- Atual/Main/_Libs.lua
+++ Atual/Main/_Libs.lua
@@ -194,8 +194,8 @@ setvocMaito = function()
   storage.magia400 = 'Yagai'
   storage.magiabijuu = 'Biju Attack'
   storage.special1 = 'Dai Yagai'
-  storage.special2 = "Open Gates Of Death"
-  storage.special3 = nil
+  storage.special2 = "Undo Cho Kosoku"
+  storage.special3 = 'Open Gates Of Death'
   storage.special4 = nil
   storage.fugaspell = nil
   storage.buff = "Open Gates Of Life"
@@ -367,75 +367,3 @@ end
 
 
 g_game.look(player)
-
-
---------------Comandos de Controle Remoto--------------
-
-followmacro = macro(100, 'Follow', function() 
-if g_game.isAttacking() or g_game.isFollowing() then return end
-  for _, spec in ipairs(getSpectators()) do
-    if spec:getName() == storage.follow3 then
-      g_game.follow(spec)
-    end
-  end
-end)
-
-configList = modules.game_bot.contentsPanel.config
-onTalk(function(name, level, mode, text, channelId, pos)
-  if channelId ~= 1 then return end
-    if text == 'CarregarHunt' then
-      configList:setCurrentOption(player:getName())
-    end
-    if text == 'CarregarTreino' then
-      configList:setCurrentOption('Treino')
-    end
-    if text == 'ligar target' then
-      TargetBot.setOn()
-    end
-    if text == 'desligar target' then
-      TargetBot.setOff()
-      Target.Id = nil
-      Target.get = nil
-      g_game.cancelAttack()
-    end
-    if text == 'pare de seguir' then
-      followmacro.setOff()
-      g_game.cancelFollow()
-    end
-    if text == 'Siga' then
-      followmacro.setOn()
-    end
-    if text == 'desbugar Follow' then
-      g_game.cancelFollow()
-    end
-end)
-
-onTalk(function(name, level, mode, text, channelId, pos)
-  if text:find('.siga ') then
-    startindex = text:find('siga')
-    endindex = text:find('!')
-    storage.follow = nil
-    storage.follow2 = nil
-    storage.follow3 = text:sub(startindex+5,endindex-1)
-  end
-end)
-
-macro(1, function()
-if player:getShield() > 0 then return end
-for _,PT in ipairs(getSpectators(posz())) do
-if PT:getEmblem() == 1 and PT:getShield() > 0 then 
- return g_game.partyJoin(PT:getId())
-end
-
-end
-
-delay(100)
-
-end)
-
-macro(2000, function()
-  if getChannelId('Party') then return end
-  if player:getShield(3) or player:getShield(2) then
-    g_game.joinChannel(1)
-  end
-end)
