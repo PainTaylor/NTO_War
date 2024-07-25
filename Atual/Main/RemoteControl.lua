8b6134777ec44df22dcad9e36524745b9fa039e4
Paintaylor Projects <mflp55@hotmail.com>
2024-07-25 10:41:43 -0300

    Update RemoteControl.lua

--- Atual/Main/RemoteControl.lua
+++ Atual/Main/RemoteControl.lua
@@ -405,7 +405,7 @@ end)
 
 remotecontrol = macro(1000, 'RemoteControl', function()end)
 onKeyDown(function(keys)
-  if modules.game_console:isChatEnabled() and remotecontrol.isOff() then return end
+  if modules.game_console:isChatEnabled() or remotecontrol.isOff() then return end
   if keys == 'P' then
     sayChannel(1, 'pare de seguir')
   end
