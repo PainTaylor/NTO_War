ccd92ab8b79149bbc4fc0926f62d814c86ebc15f
Paintaylor Projects <mflp55@hotmail.com>
2024-07-29 14:07:54 -0300

    Update chatsaga.lua

--- Atual/Main/chatsaga.lua
+++ Atual/Main/chatsaga.lua
@@ -284,257 +284,100 @@ end)
 end
 
 -----------------------------------------
-travelkonoha = function()
-	for _, spec in ipairs(getSpectators(posz())) do
-		if spec:getName() == 'Minoru' then
-			g_game.follow(spec)
-		end
-	end
-	schedule(1000, function()
-		NPC.say('hi')
-	end)
-	schedule(1500, function()
-		NPC.say('Konoha Gakure')
-	end)
-	delay(2600)
-end
 
-travelsuna = function()
-	for _, spec in ipairs(getSpectators(posz())) do
-		if spec:getName() == 'Minoru' then
-			g_game.follow(spec)
-		end
-	end
-	schedule(1000, function()
-		NPC.say('hi')
-	end)
-	schedule(1500, function()
-		NPC.say('Suna Gakure')
-	end)
-	delay(2600)
+travelforest = function()
+NPC.say('hi')
+schedule(500, function()
+NPC.say('forest')
+end)
+delay(1100)
 end
 
 travelvtakumi = function()
-	for _, spec in ipairs(getSpectators(posz())) do
-		if spec:getName() == 'Minoru' then
-			g_game.follow(spec)
-		end
-	end
-	schedule(1000, function()
-		NPC.say('hi')
-	end)
-	schedule(1500, function()
-		NPC.say('Vila Takumi')
-	end)
-	delay(2600)
-end
-
-travelMonteMyuboki = function()
-	for _, spec in ipairs(getSpectators(posz())) do
-		if spec:getName() == 'Minoru' then
-			g_game.follow(spec)
-		end
-	end
-	schedule(1000, function()
-		NPC.say('hi')
-	end)
-	schedule(1500, function()
-		NPC.say('Monte Myoboku')
-	end)
-	delay(2600)
-end
-
-travelforest = function()
-	for _, spec in ipairs(getSpectators(posz())) do
-		if spec:getName() == 'Minoru' then
-			g_game.follow(spec)
-		end
-	end
-	schedule(1000, function()
-		NPC.say('hi')
-	end)
-	schedule(1500, function()
-		NPC.say('Forest')
-	end)
-	delay(2600)
-end
-
-travelamegakure = function()
-	for _, spec in ipairs(getSpectators(posz())) do
-		if spec:getName() == 'Minoru' then
-			g_game.follow(spec)
-		end
-	end
-	schedule(1000, function()
-		NPC.say('hi')
-	end)
-	schedule(1500, function()
-		NPC.say('Amegakure no Sato')
-	end)
-	delay(2600)
-end
-
-travelsunacamp = function()
-	for _, spec in ipairs(getSpectators(posz())) do
-		if spec:getName() == 'Minoru' then
-			g_game.follow(spec)
-		end
-	end
-	schedule(1000, function()
-		NPC.say('hi')
-	end)
-	schedule(1500, function()
-		NPC.say('Suna Camp')
-	end)
-	delay(2600)
-end
-
-traveliwagakureisl = function()
-	for _, spec in ipairs(getSpectators(posz())) do
-		if spec:getName() == 'Minoru' then
-			g_game.follow(spec)
-		end
-	end
-	schedule(1000, function()
-		NPC.say('hi')
-	end)
-	schedule(1500, function()
-		NPC.say('Iwagakure Island')
-	end)
-	delay(2600)
-end
-
-travelYukigakure = function()
-	for _, spec in ipairs(getSpectators(posz())) do
-		if spec:getName() == 'Minoru' then
-			g_game.follow(spec)
-		end
-	end
-	schedule(1000, function()
-		NPC.say('hi')
-	end)
-	schedule(1500, function()
-		NPC.say('Yukigakure')
-	end)
-	delay(2600)
-end
-
-traveliwagakure = function()
-	for _, spec in ipairs(getSpectators(posz())) do
-		if spec:getName() == 'Minoru' then
-			g_game.follow(spec)
-		end
-	end
-	schedule(1000, function()
-		NPC.say('hi')
-	end)
-	schedule(1500, function()
-		NPC.say('Iwagakure')
-	end)
-	delay(2600)
-end
-
-travelvdf = function()
-	for _, spec in ipairs(getSpectators(posz())) do
-		if spec:getName() == 'Minoru' then
-			g_game.follow(spec)
-		end
-	end
-	schedule(1000, function()
-		NPC.say('hi')
-	end)
-	schedule(1500, function()
-		NPC.say('Vale do Fim')
-	end)
-	delay(2600)
-end
-
-travelkumogakure = function()
-	for _, spec in ipairs(getSpectators(posz())) do
-		if spec:getName() == 'Minoru' then
-			g_game.follow(spec)
-		end
-	end
-	schedule(1000, function()
-		NPC.say('hi')
-	end)
-	schedule(1500, function()
-		NPC.say('Kumogakure')
-	end)
-	delay(2600)
-end
-
-travelTsuki = function()
-	for _, spec in ipairs(getSpectators(posz())) do
-		if spec:getName() == 'Minoru' then
-			g_game.follow(spec)
-		end
-	end
-	schedule(1000, function()
-		NPC.say('hi')
-	end)
-	schedule(1500, function()
-		NPC.say('Tsuki no Shima')
-	end)
-	delay(2600)
+NPC.say('hi')
+schedule(500, function()
+NPC.say('vila takumi')
+end)
+delay(1100)
 end
 
 travelTFogo = function()
-	for _, spec in ipairs(getSpectators(posz())) do
-		if spec:getName() == 'Minoru' then
-			g_game.follow(spec)
-		end
-	end
-	schedule(1000, function()
-		NPC.say('hi')
-	end)
-	schedule(1500, function()
-		NPC.say('Templo do Fogo')
-	end)
-	delay(2600)
+NPC.say('hi')
+schedule(500, function()
+NPC.say('templo do fogo')
+end)
+delay(1100)
 end
 
-travelKodai = function()
-	for _, spec in ipairs(getSpectators(posz())) do
-		if spec:getName() == 'Minoru' then
-			g_game.follow(spec)
-		end
-	end
-	schedule(1000, function()
-		NPC.say('hi')
-	end)
-	schedule(1500, function()
-		NPC.say('Kodai no Shima')
-	end)
-	delay(2600)
+
+travelkonoha = function()
+NPC.say('hi')
+schedule(500, function()
+NPC.say('konoha gakure')
+end)
+delay(1100)
 end
 
-travelmoon = function()
-	for _, spec in ipairs(getSpectators(posz())) do
-		if spec:getName() == 'Minoru' then
-			g_game.follow(spec)
-		end
-	end
-	schedule(1000, function()
-		NPC.say('hi')
-	end)
-	schedule(1500, function()
-		NPC.say('Ilha da Lua')
-	end)
-	delay(2600)
+travelsunacamp = function()
+NPC.say('hi')
+schedule(500, function()
+NPC.say('suna camp')
+end)
+delay(1100)
+end
+
+travelsuna = function()
+NPC.say('hi')
+schedule(500, function()
+NPC.say('suna gakure')
+end)
+delay(1100)
+end
+
+traveliwagakure = function()
+NPC.say('hi')
+schedule(500, function()
+NPC.say('iwagakure')
+end)
+delay(1100)
+end
+
+travelamegakure = function()
+NPC.say('hi')
+schedule(500, function()
+NPC.say('amegakure no sato')
+end)
+delay(1100)
+end
+
+travelkumogakure = function()
+NPC.say('hi')
+schedule(500, function()
+NPC.say('kumogakure')
+end)
+delay(1100)
+end
+
+traveliwagakureisl = function()
+NPC.say('hi')
+schedule(500, function()
+NPC.say('Iwagakure Island')
+end)
+delay(1100)
+end
+
+travelvdf = function()
+NPC.say('hi')
+schedule(500, function()
+NPC.say('Vale do Fim')
+end)
+delay(1100)
 end
 
 travelgenbu = function()
-	for _, spec in ipairs(getSpectators(posz())) do
-		if spec:getName() == 'Minoru' then
-			g_game.follow(spec)
-		end
-	end
-	schedule(1000, function()
-		NPC.say('hi')
-	end)
-	schedule(1500, function()
-		NPC.say('Ilha Genbu')
-	end)
-	delay(2600)
+NPC.say('hi')
+schedule(500, function()
+NPC.say('ilha genbu')
+end)
+delay(1100)
 end
